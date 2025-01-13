855afcb18255c69ca1886588a02a93893fd54ce1
Kkenize <Kkenizez@gmail.com>
2025-01-13 17:41:41 -0500

    Initial Commit

---
+++ Great/GreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatApp.swift
+//  Great
+//
+//  Created by Zhejun Zhang on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
